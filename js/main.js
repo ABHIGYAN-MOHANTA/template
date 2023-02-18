b4f64a78e0697cbeed1b8a9200d6ecbe722a4e22
Abhigyan Mohanta <110360901+ABHIGYAN-MOHANTA@users.noreply.github.com>
2023-02-18 23:16:46 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-async function getACuteDogPhoto(){
-	const res = await fetch("https://dog.ceo/api/breeds/image/random")
-	const data = await res.json()
-	console.log(data)
-}
+// async function getACuteDogPhoto(){
+// 	const res = await fetch("https://dog.ceo/api/breeds/image/random")
+// 	const data = await res.json()
+// 	console.log(data)
+// }
 
 
-try {
-    getACuteDogPhoto()
-  } catch (error) {
-    console.error(error)
-    // (Note: the exact output may be browser-dependent)
-  }
+// try {
+//     getACuteDogPhoto()
+//   } catch (error) {
+//     console.error(error)
+//     // (Note: the exact output may be browser-dependent)
+//   }
   
